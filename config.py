37d7ea4d9b056c75c35a1305105467ae80edd48f
revanthkumargaming-dotcom <revanthkumargaming@gmail.com>
2026-05-28 12:47:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 API_ID = int(os.environ.get("API_ID", "20879824"))
 API_HASH = os.environ.get("API_HASH", "5f70a9a12a4bb8cc322bed62bc6007ce")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7336133398:AAFmQyV7EbtWzVXT9J1MJdz-nnih1sFSWpM")
-
-MONGO_URL = os.getenv(
-    "MONGO_URL",
-    "mongodb+srv://rupamedical:dQv9oKG7QK93BkIh@james.oufkybu.mongodb.net/?appName=james"
-)
-
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8849121451:AAFGdIoQiV6SR_LOnzW5hDgwztTYAKzl08Y")
+MONGO_URL = os.getenv( "MONGO_URL",  "mongodb+srv://rupamedical:dQv9oKG7QK93BkIh@james.oufkybu.mongodb.net/?appName=james")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
 ADMIN = 7340960697
